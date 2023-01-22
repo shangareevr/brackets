b81bb650a92343858f9df5ed22c3da41ff66e7f9
shangareevr <118281290+shangareevr@users.noreply.github.com>
2023-01-22 23:04:11 +0500

    fix: update projects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+if (str.length%2 !=0) {
+  return false;
+}
+let stack = [],
+    brackets = bracketsConfig.flat();
+for (let i = 0; i < str.length; i++) { 
+  for (let j = 0; j < brackets.length; j++) {
+    if (str[i]==brackets[j]) {
+      if (j%2==0) {
+        if (brackets[j] == brackets[j+1]) {
+          if (stack[stack.length-1]==brackets[j]) {
+            stack.pop();
+          } else {
+            stack.push(brackets[j]);
+          }
+        } else {
+          stack.push(brackets[j + 1]);
+        }
+      } else {
+        if (brackets[j]==brackets[j-1]) {
+          break;
+        } else
+        if (stack[stack.length - 1] == brackets[j]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+}
+    return (stack.length == 0)
 }
